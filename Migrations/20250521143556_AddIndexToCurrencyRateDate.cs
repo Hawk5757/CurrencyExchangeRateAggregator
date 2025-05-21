cc4ff9f7ae29259837c8afc3d0cd9855585aad2b
Hawk5757 <artemka5757@gmail.com>
2025-05-21 17:41:25 +0300

    Add Index by Date to table db CurrencyRates

---
+++ Migrations/20250521143556_AddIndexToCurrencyRateDate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CurrencyExchangeRateAggregator.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndexToCurrencyRateDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_CurrencyRates_Date",
+                table: "CurrencyRates",
+                column: "Date",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_CurrencyRates_Date",
+                table: "CurrencyRates");
+        }
+    }
+}
--- Models/CurrencyRate.cs
+++ Models/CurrencyRate.cs
@@ -1,7 +1,11 @@
+
+using System;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
 namespace CurrencyExchangeRateAggregator.Models;
 
+[Index(nameof(Date), IsUnique = true)]
 public class CurrencyRate
 {
     [Key]
